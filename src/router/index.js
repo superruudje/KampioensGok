ebc0a3dc9c6c3cb13013009c567b108a7e7ac7e2
Rudi Haamke <r.haamke@cloudseven.nl>
2024-05-26 10:45:45 +0200

    fix: production path

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import process from "eslint-plugin-vue/lib/configs/base.js";
+
+const ASSET_URL = process.env.ASSET_URL || '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}`,
+  server: {
+    port: 3434,
+  },
   plugins: [
     vue(),
   ],
