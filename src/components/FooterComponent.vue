6630eb4b7176c1c8d321cd527a13df0263321156
superruudje <rjhaamke@gmail.com>
2024-06-04 13:10:08 +0200

    feat: added hearts, just for fun.

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,6 +3,7 @@
         <div class="container-xxl">
             <div class="row">
                 <div class="col-md-4">
+                    <floating-heart/>
                     <span class="d-block text-white mb-2">Made with <span class="txt-orange">&hearts;</span>, by <a class="text-white" href="https://github.com/superruudje" target="_blank">Superruudje</a></span>
                     <a href="https://tikkie.me/pay/q2fkigvqk454cu51fdsi" target="_blank" class="btn btn-sm btn-orange rounded-0 py-2 px-3" role="button">Stuur een bedankje!</a>
                 </div>
@@ -13,6 +14,7 @@
 
 <script setup>
 
+import FloatingHeart from "@/components/floatingHeart.vue";
 </script>
 
 
---
+++ src/components/floatingHeart.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="hearts position-relative">
+        <span v-for="i in 5" class="heart float-me txt-orange">&hearts;</span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+<style lang="css" scoped>
+.hearts .heart {
+    position: absolute;
+    left: 5%;
+    bottom: 50%;
+    color: #e04b4b;
+    opacity: 0;
+    animation: heart-float 3s infinite;
+    transform-origin: 50% 100%;
+}
+
+.hearts .heart:nth-child(2) {
+    left: 10%;
+    animation-delay: 0.25s;
+}
+
+.hearts .heart:nth-child(3) {
+    left: 80%;
+    animation-delay: 1s;
+}
+
+.hearts .heart:nth-child(5) {
+    left: 60%;
+    bottom: 80%;
+    animation-delay: 1.75s;
+}
+
+.hearts .heart:nth-child(4) {
+    left: 35%;
+    animation-delay: 1.5s;
+}
+
+
+@keyframes heart-float {
+    0% {
+        opacity: 1;
+        transform: translate(0, 0) scale(0);
+    }
+
+    3% {
+        transform: translate(0, 0) scale(1);
+    }
+
+    100% {
+        opacity: 0;
+        transform: translate(0, -2em) scale(1);
+    }
+}
+</style>
