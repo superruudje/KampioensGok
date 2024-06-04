bcb0056c30c1106c9b569207f7c2a71079c6dd56
superruudje <rjhaamke@gmail.com>
2024-06-04 15:19:27 +0200

    feat: added GA4

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueApexCharts from "vue3-apexcharts";
+import VueGtag from "vue-gtag";
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +12,9 @@ import "./scss/styles.scss"
 const app = createApp(App)
 
 app.use(VueApexCharts);
+app.use(VueGtag, {
+    config: {id: import.meta.env.VITE_GA_MEASUREMENT_ID}
+}, router)
 app.use(createPinia())
 app.use(router)
 
