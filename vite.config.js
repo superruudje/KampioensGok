45c113312a3e4955fa1f9770c41dd938f30f9e17
superruudje <rjhaamke@gmail.com>
2025-05-01 16:46:56 +0200

    fix: ref to public assets.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const ASSET_URL = process.env.ASSET_URL || '';
+const ASSET_URL = process.env.NODE_ENV === 'production'
+    ? '/KampioensGok/'  // Match your GitHub Pages subdirectory
+    : '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
