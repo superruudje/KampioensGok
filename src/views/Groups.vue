106ac8fd793fedcc9e66cb9db4612e1ede2ea52e
superruudje <rjhaamke@gmail.com>
2025-12-05 23:27:19 +0100

    fix: groups

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -136,7 +136,7 @@ function openPouleDetails(poule: string) {
  * @param teamName
  */
 function isTeam(teamName: string) {
-    return teams.value.some(t => t.short_name === teamName)
+    return teams.value.some(t => t.id === teamName)
 }
 
 </script>
