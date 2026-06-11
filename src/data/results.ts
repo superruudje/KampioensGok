fc24c2ff87754eedbce3ef35ba2f795fb71c3bf8
superruudje <rjhaamke@gmail.com>
2026-06-11 23:05:46 +0200

    chore: match #1

--- src/data/results.ts
+++ src/data/results.ts
@@ -10,8 +10,59 @@ export const results: Record<
         | "events"
     >
 > = {
-    // 1: {
-    //     result: [2, 1],
-    //     events: []
-    // }
+    1: {
+        result: [2, 0],
+        events: [
+            {
+                minute: 9,
+                type: 'goal',
+                player: 'Julian Quinones',
+                team: 'MEX',
+                assist: 'Erik Lira'
+            },
+            {
+                minute: 17,
+                type: 'yellow_card',
+                player: 'Teboho Mokoena',
+                team: 'RSA'
+            },
+            {
+                minute: 23,
+                type: 'yellow_card',
+                player: 'Brian Gutierrez',
+                team: 'MEX'
+            },
+            {
+                minute: 49,
+                type: 'red_card',
+                player: 'Sphephelo Sithole',
+                team: 'RSA'
+            },
+            {
+                minute: 67,
+                type: 'goal',
+                player: 'Raul Jimenez',
+                team: 'MEX',
+                assist: 'Roberto Alvarado'
+            },
+            {
+                minute: 74,
+                type: 'yellow_card',
+                player: 'Nkosinathi Sibisi',
+                team: 'RSA'
+            },
+            {
+                minute: 84,
+                type: 'red_card',
+                player: 'Themba Zwane',
+                team: 'RSA'
+            },
+            {
+                minute: 92,
+                type: 'red_card',
+                player: 'Cesar Montes',
+                team: 'MEX'
+            },
+        ]
+    }
 };
