7e1302381a94489138b6728311f5d90c3f492125
superruudje <158449657+superruudje@users.noreply.github.com>
2026-06-15 09:17:55 +0200

    chore: match #9 #12

--- src/data/results.ts
+++ src/data/results.ts
@@ -351,6 +351,10 @@ export const results: Record<
             },
         ]
     },
+    9: {
+        result: [1, 0],
+        events: []
+    },
     10: {
         result: [7, 1],
         events: [
@@ -407,6 +411,10 @@ export const results: Record<
     },
     11: {
         result: [2, 2],
+        events: []
+    },
+    12: {
+        result: [5, 1],
         events: []
     }
 };
